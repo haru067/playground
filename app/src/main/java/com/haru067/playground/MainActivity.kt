0909d55bfceeb48d942190a5621149fd0b135919
haru067 <haru.pdf@gmail.com>
2023-07-31 23:18:19 +0900

    Add button example

--- app/src/main/java/com/haru067/playground/MainActivity.kt
+++ app/src/main/java/com/haru067/playground/MainActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.haru067.playground.components.ButtonExample
 import com.haru067.playground.components.CarouselExample
 import com.haru067.playground.ui.theme.PlaygroundTheme
 
@@ -22,10 +27,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PlaygroundTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Column {
-                        CarouselExample(Modifier.fillMaxWidth().height(200.dp))
+                    Column(Modifier.verticalScroll(rememberScrollState())) {
+                        CarouselExample(
+                            Modifier
+                                .fillMaxWidth()
+                                .height(200.dp)
+                        )
+                        Spacer(modifier = Modifier.height(32.dp))
+                        ButtonExample()
                     }
                 }
             }
---
+++ app/src/main/java/com/haru067/playground/components/ButtonExample.kt
@@ -0,0 +1,51 @@
+package com.haru067.playground.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalMinimumTouchTargetEnforcement
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ButtonExample(modifier: Modifier = Modifier) {
+    Row(modifier) {
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .border(width = 1.dp, color = Color.Blue)
+        ) {
+            Text("Default")
+        }
+        Spacer(modifier = Modifier.width(16.dp))
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .border(width = 1.dp, color = Color.Blue)
+                .heightIn(min = 36.dp)
+        ) {
+            Text("36dp")
+        }
+        Spacer(modifier = Modifier.width(16.dp))
+
+        CompositionLocalProvider(LocalMinimumTouchTargetEnforcement provides false) {
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .border(width = 1.dp, color = Color.Blue)
+                    .heightIn(min = 36.dp)
+            ) {
+                Text("36.dp")
+            }
+        }
+    }
+}
