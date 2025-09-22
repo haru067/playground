f37501d821953cebed5f5a94d68d9b5aef5b2e4a
haru067 <haru.pdf@gmail.com>
2025-09-22 10:08:16 +0900

    Fix build error

--- app/src/main/java/com/haru067/playground/components/ButtonExample.kt
+++ app/src/main/java/com/haru067/playground/components/ButtonExample.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LocalMinimumTouchTargetEnforcement
+import androidx.compose.material3.LocalMinimumInteractiveComponentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -21,37 +21,37 @@ import com.haru067.playground.R
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ButtonExample(modifier: Modifier = Modifier) {
-    Row(modifier) {
-        Button(
-            onClick = {},
-            modifier = Modifier
-                .border(width = 1.dp, color = Color.Blue)
-        ) {
-            Text("Default")
-        }
-        Spacer(modifier = Modifier.width(16.dp))
-        Button(
-            onClick = {},
-            modifier = Modifier
-              .border(width = 1.dp, color = Color.Blue)
-              .heightIn(min = 36.dp)
-        ) {
-            Text("36dp")
-          Text(stringResource(R.string.test_tex))
-        }
-        Spacer(modifier = Modifier.width(16.dp))
-
-        CompositionLocalProvider(LocalMinimumTouchTargetEnforcement provides false) {
-            Button(
-                onClick = {},
-                modifier = Modifier
-                  .border(width = 1.dp, color = Color.Blue)
-                  .heightIn(min = 36.dp)
-            ) {
-                Text("36.dp")
-            }
-        }
+  Row(modifier) {
+    Button(
+      onClick = {},
+      modifier = Modifier
+        .border(width = 1.dp, color = Color.Blue)
+    ) {
+      Text("Default")
     }
+    Spacer(modifier = Modifier.width(16.dp))
+    Button(
+      onClick = {},
+      modifier = Modifier
+        .border(width = 1.dp, color = Color.Blue)
+        .heightIn(min = 36.dp)
+    ) {
+      Text("36dp")
+      Text(stringResource(R.string.test_tex))
+    }
+    Spacer(modifier = Modifier.width(16.dp))
+
+    CompositionLocalProvider(LocalMinimumInteractiveComponentSize provides 0.dp) {
+      Button(
+        onClick = {},
+        modifier = Modifier
+          .border(width = 1.dp, color = Color.Blue)
+          .heightIn(min = 36.dp)
+      ) {
+        Text("36.dp")
+      }
+    }
+  }
 }
 
 @Preview
